b10bf535b234aa409e30036acd7a75aaf6955985
solar <love0and0life@gmail.com>
2016-05-13 10:27:07 +0800

    商品列表价格的间距调整

--- es/content.js
+++ es/content.js
@@ -24,7 +24,7 @@ export function generateGood (message, owner) {
                             <img class="good-banner" src="${message.picture[0]}" alt="首图">
                         </div>
                         <div class="ui-col ui-col-2 ui-row-flex ui-row-flex-ver good-content">
-                            <div class="ui-col-2" style="height: 24px;">
+                            <div class="ui-col-2" style="height: 61px;">
                                 <h1 class="ui-nowrap-multi ui-flex-pack-start ui-col-3 good-topic">${message.describe}</h1>
                             </div>
                             <h2 class="ui-col good-price" style="height: 24px;">¥${message.price}<i class="good-cost">¥${message.cost}</i></h2>
